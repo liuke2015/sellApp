ad7e7298569ef83fe67041ad0c5ced86cf81b1cd
liuke2015 <376060613@qq.com>
2019-03-29 17:28:11 +0800

    main.js:引入假数据

--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import App from './App'
 import router from './router'
 
 Vue.config.productionTip = false
+//引入假数据
+Vue.prototype.appData = require('@static/data.json')
+Vue.prototype.seller = Vue.prototype.appData.seller
+Vue.prototype.goods = Vue.prototype.appData.goods
+Vue.prototype.ratings = Vue.prototype.appData.ratings
 
 /* eslint-disable no-new */
 new Vue({
